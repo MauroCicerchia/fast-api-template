56ec9613a81e9122c410e1e93fe839828843675c
Mauro Cicerchia <maurocicerchia98@gmail.com>
2022-03-12 11:02:54 -0300

    feat: datetime endpoint and counter mock

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from datetime import datetime
+from schemas import *
 
 app = FastAPI()
 
+counter = 0
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/datetime")
+def process_datetime(options: Optional[DatetimeOptions] = None):
+    registerCall()
+    if(options and options.alternative_format):
+        return {"datetime": datetime.now().strftime("%Y-%d-%m")}
+    else:
+        return {"datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+
+@app.get("/count")
+def get_count():
+    registerCall()
+    return {"count": counter}
+
+
+def registerCall():
+    global counter
+    counter += 1
